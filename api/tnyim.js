8cbaf3adb04abaec266e99bf18b77efc22c8cdfc
MR.REMBO <160395549+MRREMBO@users.noreply.github.com>
2024-10-18 11:23:38 +0700

    Add files via upload

---
+++ api/tnyim.js
@@ -0,0 +1,33 @@
+module.exports = function(app) {
+
+async function tnyim(url) {
+        try {
+            const response = await fetch(`https://tny.im/yourls-api.php?format=json&action=shorturl&url=${url}`);
+            const result = await response.json();
+            return result.shorturl;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+//Contoh Penggunaan
+app.get('/tnyim', async (req, res) => {
+    try {
+      const { text } = req.query;
+      if (!text) {
+        return res.status(400).json({ error: 'Parameter "text" Tidak Ditemukan, Tolong Masukkan Perintah' });
+      }
+      const response = await fetchContent(text);
+      res.status(200).json({
+        status: 200,
+        creator: "Ditzz",
+        data: response
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+};
+
+
+
