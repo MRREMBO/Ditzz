6d80c004658af302a6e6aded4d51f0d2679ac193
MR.REMBO <160395549+MRREMBO@users.noreply.github.com>
2024-10-19 11:10:11 +0700

    Update umaruai.js

--- ai/umaruai.js
+++ ai/umaruai.js
@@ -38,17 +38,21 @@ module.exports = function(app) {
             m.reply('Terjadi kesalahan.');
         }
     }
-    app.get('/umaruai', async (req, res) => {
-    const { text } = req.query;
-    if (!text) {
-        return res.status(400).json({ error: ' parameter is required' });
-    }
-
-    try {
-        const results = await umaruai(text, m);
-        res.status(200).json(results);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching book data' });
-    }
-});
-};
+ app.get('/umaruai', async (req, res) => {
+      try {
+        const { text } = req.query;
+        if (!text) {
+          return res.status(400).json({ error: 'Parameter "text" tidak ditemukan.' });
+        }
+        const response = await umaruai(text);
+        res.status(200).json({
+          status: 200,
+          creator: "Ditzz",
+          data: response,
+        });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+  };
+  
