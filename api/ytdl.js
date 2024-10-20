4c3de81da2c6212d8c73eded3e45bbd34b986844
MR.REMBO <160395549+MRREMBO@users.noreply.github.com>
2024-10-20 16:54:11 +0700

    Add files via upload

---
+++ api/ytdl.js
@@ -0,0 +1,40 @@
+module.exports = function(app) {
+
+async function ytdl(url) {
+    const response = await fetch('https://shinoa.us.kg/api/download/ytdl', {
+        method: 'POST',
+        headers: {
+            'accept': '*/*',
+            'api_key': 'free',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: url
+        })
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+app.get('/ytdl', async (req, res) => {
+      try {
+        const { text } = req.query;
+        if (!text) {
+          return res.status(400).json({ error: 'Parameter "url" tidak ditemukan.' });
+        }
+        const response = await ytdl(url);
+        res.status(200).json({
+          status: 200,
+          creator: "Ditzz",
+          data: response,
+        });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+    });
+  };
